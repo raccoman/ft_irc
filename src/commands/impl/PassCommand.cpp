ed450f2953468b1b7e8af4f6c1950b2ef32c7403
rboldini <boldiniroberto@me.com>
2022-03-12 15:59:30 +0100

    Fix pass command

--- src/commands/impl/PassCommand.cpp
+++ src/commands/impl/PassCommand.cpp
@@ -16,7 +16,7 @@ void PassCommand::execute(Client *client, std::vector<std::string> arguments)
 		return;
 	}
 
-	if (_server->getPassword() != arguments[0].substr(1, arguments[0].length())) {
+	if (_server->getPassword() != arguments[0].substr(arguments[0][0] == ':' ? 1 : 0, arguments[0].length())) {
 		client->sendMessage(ERR_PASSWDMISMATCH);
 		return;
 	}
